a9657a7889a63303237ea150d942d4b12f8e8260
Asnake Assefa <30431907+asnakeassefa@users.noreply.github.com>
2023-04-27 21:18:07 +0300

    added specialNumber.py

--- specialNumber.py
+++ specialNumber.py
@@ -1,18 +1,11 @@
 total = int(input())
 
-ans = 0
-
 for _ in range(total):
-    len = input()
-    nums = list(map(int,input().split()))
-    for i,num in enumerate(nums):
-        temp = 0
-        for j,num2 in enumerate(nums):
-            if j == i:
-                continue
-            else:
-                temp ^= num2
-        if temp == num:
-            ans = num
-            break
-    print(ans)
+    base,num = map(int,input().split())
+    i = 0
+    ans = 0
+    while num:
+        ans += (base ** i) * (num & 1)
+        num >>= 1
+        i += 1
+    print(ans%((10 ** 9) + 7))
